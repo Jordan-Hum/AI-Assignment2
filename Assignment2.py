5ebda608040d1fb37fa33616dd023a1872d8bed2
JordanHum <jhum268@gmail.com>
2020-06-12 00:54:46 -0400

    task 1 working without remove_words or vocabulary

--- Assignment2.py
+++ Assignment2.py
@@ -4,22 +4,98 @@
 # For COMP 472 Section IX – Summer 2020
 # --------------------------------------------------------
 
-#Library Imports
+# Library Imports
 import pandas as pd
 import nltk
 
-#nltk.download()
 
-#read csv files
+# nltk.download()
+
+# FUNCTIONS
+# ----------------------------------------
+def sortData():
+    for index, row in df.iterrows():
+        if '2018' in row['created_at']:
+            year18.append(row)
+            title18.append(row['title'].lower())
+            postType18.append(row['post_type'])
+
+    for i in range(len(postType18)):
+        if postType18[i] == 'story':
+            for word in nltk.word_tokenize(title18[i]):
+                storyList.append(word)
+                if word not in allWords:
+                    allWords.append(word)
+        if postType18[i] == 'ask_hn':
+            for word in nltk.word_tokenize(title18[i]):
+                askList.append(word)
+                if word not in allWords:
+                    allWords.append(word)
+        if postType18[i] == 'show_hn':
+            for word in nltk.word_tokenize(title18[i]):
+                showList.append(word)
+                if word not in allWords:
+                    allWords.append(word)
+        if postType18[i] == 'poll':
+            for word in nltk.word_tokenize(title18[i]):
+                pollList.append(word)
+                if word not in allWords:
+                    allWords.append(word)
+
+
+def sortFreq():
+    storyFreq = nltk.FreqDist(storyList)
+    askFreq = nltk.FreqDist(askList)
+    showFreq = nltk.FreqDist(showList)
+    pollFreq = nltk.FreqDist(pollList)
+    for word in allWords:
+        # check frequency of word in story posts
+        wordFreq = storyFreq[word]
+        storyDict[word] = (wordFreq + 0.5) / len(storyList)
+        # check frequency of word in ask_hn posts
+        wordFreq = askFreq[word]
+        askDict[word] = (wordFreq + 0.5) / len(askList)
+        # check frequency of word in show_hn posts
+        wordFreq = showFreq[word]
+        showDict[word] = (wordFreq + 0.5) / len(showList)
+        # check frequency of word in poll posts
+        # wordFreq = pollFreq[word]
+        # pollDict[word] = (wordFreq + 0.5) / len(pollList)
+    file = open('model-2018.txt', 'w', encoding='utf-8')
+    wordCounter = 0
+    for word in sorted(allWords):
+        if word.isalnum():
+            wordCounter += 1
+            file.write(str(wordCounter) + '  ' + word + '  ' + str(storyFreq[word]) + '  ' + str(storyDict[word]) + '  ' +
+                       str(askFreq[word]) + '  ' + str(askDict[word]) +
+                       '  ' + str(showFreq[word]) + '  ' + str(showDict[word]) + "\n")
+    file.close()
+
+
+# MAIN
+# ----------------------------------------------
+
+# read csv files
 df = pd.read_csv('hns_2018_2019.csv')
 df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+# initialize lists and dictionaries used
 year18 = []
 title18 = []
-for index, row in df.iterrows():
-    if '2018' in row['created_at']:
-        year18.append(row)
-        title18.append(row['title'].lower())
+postType18 = []
+storyList = []
+askList = []
+showList = []
+pollList = []
+allWords = []
+storyDict = {}
+askDict = {}
+showDict = {}
+pollDict = {}
 
-title18 = [nltk.word_tokenize(words) for words in title18]
-print(title18)
+# function calls
+sortData()
+sortFreq()
 
+# PRINT CHECKS
+# -------------------
